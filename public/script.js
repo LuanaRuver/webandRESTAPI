749e059163c5117bd1f0807be86521234b17d474
Luana Ruver <luana_ruver@hotmail.com>
2024-08-26 22:49:58 -0300

    atualização button script

--- public/script.js
+++ public/script.js
@@ -1,7 +1,8 @@
-document.getElementById('Sing').addEventListener('click', () => {
+const buttons = document.querySelectorAll('.Sing');
+buttons.forEach(button => { button.addEventListener('click', () => {
     const formLogin = document.getElementById('logar');
     formLogin.style.display = 'none';
     const formCriarUsuario = document.getElementById('criar_usuario');
     formCriarUsuario.style.display = 'flex'; 
-});
+})});
 
